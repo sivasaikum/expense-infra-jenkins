da138db1c0b1f9de8315493cd63f6547cce874be
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-12 08:51:49 +0530

    jenkins

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
 
     backend "s3" {
-      bucket = "sai-tf-state-remote"
+      bucket = "sai-tf-state-remote2"
       key = "jenkins-terraform-expense-vpc" # yo should have unique keys within the bucket, same keys should not be used in others repo or tf floders
       region = "us-east-1"
       dynamodb_table = "sai-tf-state-locking"
