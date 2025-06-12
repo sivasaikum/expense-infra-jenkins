5e00fed573dad0a403ee69bf62622823012c7489
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-12 13:12:02 +0530

    jenkins

---
+++ 70-ingress-alb/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    alb_ingress_sg_id = data.aws_ssm_parameter.alb_ingress_sg_id.value
+    ingress_alb_certificate_arn = data.aws_ssm_parameter.ingress_alb_certificate_arn.value
+    resource_name = "${var.project}-${var.environment}-ingress"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+}
