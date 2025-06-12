97acb637c5589ab149cd79f9b05d4e3f7dc5538e
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-12 12:50:03 +0530

    jenkins

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -36,6 +36,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+    create_before_destroy = true
   }
 
   eks_managed_node_groups = {
@@ -58,7 +59,9 @@ module "eks" {
      }
    }
 
-   green = {
+  }
+  eks_managed_node_groups = {
+    green = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.xlarge"]
@@ -76,7 +79,6 @@ module "eks" {
         AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
      }
    }
-
   }
 
   tags = merge (
