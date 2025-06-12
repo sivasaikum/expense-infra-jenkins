a36ca2a28eeae0eccedbcee165054f4e043ff2ec
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-12 12:51:05 +0530

    jenkins

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -60,7 +60,7 @@ module "eks" {
    }
 
   }
-  eks_managed_node_groups = {
+  eks_managed_node_groups_green = {
     green = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
