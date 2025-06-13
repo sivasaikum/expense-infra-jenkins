49b3a48ba985fa0d56727224f7cc314982725bf4
sivasaikum <sivasaikumarallampati@gmail.com>
2025-06-13 10:56:11 +0530

    jenkins

--- 40-eks/provider.tf
+++ 40-eks/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "s3" {
       bucket = "sai-tf-state-remote2"
-      key = "jenkins2-terraform-expense-cluster" # you should have unique keys within the bucket, same keys should not be used in others repo or tf floders
+      key = "jenkins-terraform-expense-cluster" # you should have unique keys within the bucket, same keys should not be used in others repo or tf floders
       region = "us-east-1"
       dynamodb_table = "sai-tf-state-locking"
     }
